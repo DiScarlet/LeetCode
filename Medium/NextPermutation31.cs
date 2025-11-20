e0d54f0e35b8c45bd0e31f0e71dd29c03b299b8b
Diana Antoniuk <136202185+DiScarlet@users.noreply.github.com>
2025-11-20 18:40:17 +0100

    31. Next Permutation
    Medium level task 31. Next Permutation

---
+++ Medium/NextPermutation31.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            int[] nums = { 1, 3, 2 };
+            NextPermutation(nums);
+            Print(nums);
+        }
+
+        public static void Print(int[] nums)
+        {
+            foreach (var item in nums)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static void NextPermutation(int[] nums)
+        {
+            int length = nums.Length;
+
+            bool wasSorted = false;
+            for (int i = length - 1; i > 0; i--)
+            {
+                int current = nums[i];
+                int precurrent = nums[i - 1];
+                if(precurrent < current)
+                {
+                    wasSorted = true;
+                    (nums[i - 1], nums[FindIndOfNextBiggest(nums, i - 1)]) = (nums[FindIndOfNextBiggest(nums, i - 1)], nums[i - 1]);
+                    Array.Sort(nums, i, length - i);
+                    break;
+                }
+            }
+
+            if(!wasSorted)
+                Array.Reverse(nums);
+        }
+
+        public static int FindIndOfNextBiggest(int[] nums, int startInd)
+        {
+            int comparable = nums[startInd];
+            int biggerInd = startInd + 1;
+
+            int i = startInd + 1;
+            for (; i < nums.Length; i++)
+            {
+                int current = nums[i];
+                if (current < nums[biggerInd] && current > comparable)
+                {
+                    biggerInd = i;
+                }
+            }
+
+            return biggerInd;
+        }
+    }
+}
