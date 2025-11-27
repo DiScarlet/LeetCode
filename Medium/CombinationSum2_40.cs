6f74a72a42378de8de6a9e5f9bd82de0ca68cb23
Diana Antoniuk <136202185+DiScarlet@users.noreply.github.com>
2025-11-27 21:38:57 +0100

    40. Combination Sum II
    Medium level task 40. Combination Sum II. Solution not accepted - Time Limit Exceeded
    124 / 176 testcases passed
    Last Executed Input:
    candidates = [14,6,25,9,30,20,33,34,28,30,16,12,31,9,9,12,34,16,25,32,8,7,30,12,33,20,21,29,24,17,27,34,11,17,30,6,32,21,27,17,16,8,24,12,12,28,11,33,10,32,22,13,34,18,12]
    target = 27

---
+++ Medium/CombinationSum2_40.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Print(CombinationSum2(new int[] { }, 27));
+        }
+
+        public static void Print(IList<IList<int>> res)
+        {
+            foreach (var list in res)
+            {
+                foreach (var item in list)
+                {
+                    Console.Write(item);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static IList<IList<int>> CombinationSum2(int[] candidates, int target)
+        {
+            Array.Sort(candidates);
+            IList<IList<int>> result = new List<IList<int>>();
+            return Backtrack(candidates, new List<int>(), 0, 0, target, result);
+
+        }
+        public static IList<IList<int>> Backtrack(int[] candidates, List<int> current, int start, int sum, int target, IList<IList<int>> result)
+        {
+            if (sum == target)
+            {
+                result.Add(new List<int>(current));
+                return result;
+            }
+
+            for(int i = start ; i < candidates.Length; i++)
+            {
+                int candidate = candidates[i];
+
+                if(i != 0)
+                {
+                    if (candidate == candidates[i - 1] && i > start)
+                        continue;
+                }
+
+                current.Add(candidate);
+                Backtrack(candidates, current, i + 1, sum + candidate, target, result);
+                current.RemoveAt(current.Count - 1);
+            }
+            return result;
+        }
+    }
+}
